d7102aeecb70641ff789b3c1a1b1abb706714860
Prakashom18 <shahomprakash097@gmail.com>
2025-12-27 09:33:02 +0545

    Added profile pic

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const postModel = require('./models/postModel')
 const multerconfig = require('./config/userconfig')
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
